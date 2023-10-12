adbe5a5fecad769375aa955f4970a28a1a97cc40
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-12 19:31:41 -0300

    Criado Enum e Reducer (sem tipagem)

--- multiform/src/contexts/FormContext.tsx
+++ multiform/src/contexts/FormContext.tsx
@@ -2,3 +2,27 @@ import { createContext } from "vm";
 
 
 const FormContext = createContext(undefined);
+
+enum FormActions {
+    setCurrentStep,
+    setName,
+    setLevel,
+    setEmail,
+    setGithub
+}
+const FormReducer = (state, action) => {
+    switch(action.type) {
+        case FormActions.setCurrentStep:
+            return {...state, curretStep: action.payload};
+        case FormActions.setName:
+            return {...state, name: action.payload};
+        case FormActions.setLevel:
+            return {...state, level: action.payload};
+        case FormActions.setEmail:
+            return {...state, email: action.payload};
+        case FormActions.setGithub:
+            return {...state, github: action.payload};
+        default:
+            return state;
+    }
+} 
