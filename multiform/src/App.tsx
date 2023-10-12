6c090005a79022f03891c2e40e5dcac68582cee9
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-12 19:05:31 -0300

    importando todas as pages para o App.tsx

--- multiform/src/App.tsx
+++ multiform/src/App.tsx
@@ -1,9 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { FormStep1 } from './pages/FormStep1';
+import { FormStep2 } from './pages/FormStep2';
+import { FormStep3 } from './pages/FormStep3';
 
 
-const App = () => {
+function App() {
   return (
-    <div>...</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<FormStep1 />} />
+        <Route path='/step2' element={<FormStep2 />} />
+        <Route path='/ste33' element={<FormStep3 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
