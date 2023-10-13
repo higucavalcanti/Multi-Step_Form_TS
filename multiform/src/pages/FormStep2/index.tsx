0fc8674506c9f10c08cb4bb183e2f8f9aafc70c8
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-13 12:16:13 -0300

    FormStep2
    (Copiado do FOrmStep1 para facilitar e ter a base das paginas

--- multiform/src/pages/FormStep2/index.tsx
+++ multiform/src/pages/FormStep2/index.tsx
@@ -1,9 +1,56 @@
-import * as C from "./styles";
+import { useNavigate } from 'react-router-dom';
+import * as C from './styles';
+import { Theme } from '../../components/Theme';
+import { FormActions, useForm } from '../../contexts/FormContext';
+import { ChangeEvent, useEffect } from 'react';
 
-export const FormStep2 = () => {
+export const FormStep1 = () => {
+  const navigate = useNavigate();
+  const { state, dispatch } = useForm();
+
+  useEffect(() => {
+    dispatch({
+        type: FormActions.setCurrentStep,
+        payload: 1
+    });
+  }, []);
+  
+  const handleNextStep = () => {
+    if(state.name !== '') {
+        navigate('/page2');
+    } else {
+        alert("Preencha os dados");
+    }
+  };
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+        type: FormActions.setName,
+        payload: e.target.value
+    });
+  }
+
     return (
-        <C.Container>
-            Passo 2
-        </C.Container>
+        <Theme>
+            <C.Container>
+                <p>Passo 1/3</p>
+                <h1>Vamos começar com seu nome!</h1>
+                <p>Preencha o campo abaixo com seu nome completo</p>
+
+                <hr />
+
+                <label>
+                    Seu nome completo
+                    <input 
+                        type="text"
+                        autoFocus
+                        value={state.name}
+                        onChange={handleNameChange}
+                    />
+                </label>
+
+                <button onClick={handleNextStep}>Próximo</button>
+            </C.Container>
+        </Theme>
     );
-}
+};
