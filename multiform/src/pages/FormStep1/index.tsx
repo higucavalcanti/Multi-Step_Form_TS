c0e47d74da82904d5a9d9d6391cd4ce91dbb597e
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-13 12:06:42 -0300

    Criando Função para o nome e useEffect
    a função salva o que for digitado no Context e o useEffect servepara marca em qual pagina esta (será trabalhado futuramente)

--- multiform/src/pages/FormStep1/index.tsx
+++ multiform/src/pages/FormStep1/index.tsx
@@ -1,13 +1,30 @@
 import { useNavigate } from 'react-router-dom';
 import * as C from './styles';
 import { Theme } from '../../components/Theme';
+import { FormActions, useForm } from '../../contexts/FormContext';
+import { ChangeEvent, useEffect } from 'react';
 
 export const FormStep1 = () => {
   const navigate = useNavigate();
+  const { state, dispatch } = useForm();
 
+  useEffect(() => {
+    dispatch({
+        type: FormActions.setCurrentStep,
+        payload: 1
+    });
+  }, []);
+  
   const handleNextStep = () => {
     navigate('/page2');
   };
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+        type: FormActions.setName,
+        payload: e.target.value
+    });
+  }
 
     return (
         <Theme>
@@ -23,6 +40,8 @@ export const FormStep1 = () => {
                     <input 
                         type="text"
                         autoFocus
+                        value={state.name}
+                        onChange={handleNameChange}
                     />
                 </label>
 
