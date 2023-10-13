b39bc0508b316915b0ecaec5e577dd563c614001
Higor Cavalcanti <cavalcanti.higor1@gmail.com>
2023-10-13 09:55:15 -0300

    colocando frase abaixo do titulo do Header

--- multiform/src/components/Header/index.tsx
+++ multiform/src/components/Header/index.tsx
@@ -4,6 +4,7 @@ export const Header = () => {
     return (
         <C.Container>
             <h1>Cadastro de Desenvolvedor</h1>
+            <p>Faça seu cadastro na lista seguindo todos os passos</p>
         </C.Container>
     );
 }
